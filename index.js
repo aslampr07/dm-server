2bc00a30d1f0c9ffef23f2faa403c2e1374e0848
Aslam Rasheed <aslampr07@outlook.com>
2019-01-30 11:43:39 +0530

    Edited index

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
  const app = express();
 
+
 app.get("/test", function(req, res){
     let name = req.query.name
-    res.send("Hello World " + name)
+    res.send(`Hai ${name}`);
 });
 
  app.listen(8000);
