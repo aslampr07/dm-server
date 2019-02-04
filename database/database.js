82fc03ca60f27e43b1dad54ae6f3c76cbd0c687c
Aslam Rasheed <aslampr07@outlook.com>
2019-02-04 19:00:41 +0530

    Create first basic service endpoint

---
+++ database/database.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const fs = require('fs')
+
+console.log(__dirname);
+
+const data = fs.readFileSync(__dirname+"/../config/database.json");
+let cred = JSON.parse(data)
+
+const pool = mysql.createPool({
+    connectionLimit : 100,
+    host : cred.host,
+    user : cred.user,
+    password : cred.password,
+    database : cred.database
+});
+
+exports.pool = pool;
--- index.js
+++ index.js
@@ -1,11 +1,30 @@
- const express = require('express');
+const express = require('express');
+const pool = require('./database/database').pool;
+const mysql = require('mysql')
+const app = express();
 
- const app = express();
+app.use(express.urlencoded({extended:true}));
 
+app.post("/volunteer/register", function (req, res) {
+    let name = req.body.name;
+    let phone = req.body.phone;
+    let lat = Number(req.body.lat);
+    let lon = Number(req.body.lon);
 
-app.get("/test", function(req, res){
-    let name = req.query.name
-    res.send(`Hai ${name}`);
+    let sql = mysql.format("INSERT INTO Volunteer(name, phone, latitude, longitude) VALUES (?, ?, ?, ?)", [name, phone, lat, lon]);
+
+    pool.query(sql, function (err, rows) {
+        if (err) {
+            throw err;
+        }
+        console.log("data inserted");
+        let response = {
+            "success": true
+        }
+        res.send(response)
+    });
 });
 
- app.listen(8000);
+app.listen(8000, () => {
+    console.log("Server is listening")
+});
