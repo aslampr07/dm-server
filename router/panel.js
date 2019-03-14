3f7f897abbae4acff5ef279b5338933dd450ae36
Aslam Rasheed <aslampr07@outlook.com>
2019-03-14 14:14:18 +0530

    added news posting service

--- router/panel.js
+++ router/panel.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const crypto = require('crypto')
 const pool = require("../database/database").pool
 
 const router = express.Router()
@@ -29,7 +30,32 @@ router.get("/news", (req, res) => {
         }
         res.json(response)
     });
-
 });
+
+router.post("/news", (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let priority = number(req.body.priority);
+
+    let sql = mysql.format("INSERT INTO news(priority, heading, body, creationTime) VALUES(?, ?, ?, NOW())", [priority, title, body]);
+    pool.query(sql, (err, rows) => {
+        if(err){
+            throw err;
+        }
+        let id = rows.insertId;
+        let code = crypto.randomBytes(3).toString('HEX');
+        let sql = mysql.format("UPDATE news set code = ? WHERE ID = ?", [code, id])
+        pool.query(sql, (err, rows) => {
+            if(err){
+                throw err;
+            }
+            let response = {
+                "success": true,
+                "code": code
+            }
+            res.json(response);
+        })
+    })
+})
 
 module.exports = router
